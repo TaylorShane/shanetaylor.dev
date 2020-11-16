daabbd9ae49e8758311a0e29f67071d63e55da7b
Shane Taylor <shanemailer@gmail.com>
2020-11-15 22:19:35 -0600

    initial commit. copying old html to new ang site

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { WhoComponent } from './who/who.component';
+import { WhatComponent } from './what/what.component';
+import { WorkExamplesComponent } from './work-examples/work-examples.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    WhoComponent,
+    WhatComponent,
+    WorkExamplesComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/work-examples/work-examples.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-work-examples',
+  templateUrl: './work-examples.component.html',
+  styleUrls: ['./work-examples.component.css'],
+})
+export class WorkExamplesComponent {
+  constructor() {}
+
+}
