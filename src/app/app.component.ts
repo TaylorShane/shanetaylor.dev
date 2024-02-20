528315d91fb61266ee9b77fd6a5febf058cad48f
Shane <shanemailer@gmail.com>
2024-02-19 18:20:59 -0600

    fix: simpligy app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute, UrlSegment } from '@angular/router';
-import { Subject, takeUntil } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'st-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnDestroy, OnInit {
+export class AppComponent {
   title = 'Shane Taylor';
-  currentRoute: UrlSegment[];
-
-  private readonly destroy$ = new Subject<void>();
-
-  constructor(private route: ActivatedRoute) {}
-
-  ngOnInit(): void {
-    this.route.url.pipe(takeUntil(this.destroy$)).subscribe((route) => {
-      this.currentRoute = route;
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
 }
