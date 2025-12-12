7a9365b92149c462e32d65e31b6e0a7b5bcc9f92
Shane Taylor <shanetaylor@siemens.com>
2025-12-12 09:20:00 -0600

    feat: add readonly token to requests

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  gitHubToken: process.env['GH_PAT'] || 'failed-to-load-token'
+  gitHubToken: process.env.NG_APP_GH_PAT || 'failed-to-load-token'
 };
