2b8d94bef3a9fc0b110474966db60f03b6932f0a
Shane Taylor <shanetaylor@siemens.com>
2025-12-31 10:36:53 -0600

    fix: coverage badge

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     include: ['src/**/*.spec.ts'],
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'html'],
+      reporter: ['text', 'html', 'lcov'],
       exclude: ['**/*github-backup-data.ts']
     }
   }
